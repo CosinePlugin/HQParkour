27892345fd763ab58887384b626d02f0a6a2ee84
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2023-11-22 02:20:44 +0900

    feat: days 로직 추가

--- src/main/kotlin/kr/cosine/parkour/extension/TimeExtension.kt
+++ src/main/kotlin/kr/cosine/parkour/extension/TimeExtension.kt
@@ -1,12 +1,15 @@
 package kr.cosine.parkour.extension
 
 internal fun Long.toKoreanTimeFormat(): String {
-    val hours = this / (1000 * 60 * 60)
-    val minutes = this / (1000 * 60)
-    val seconds = (this / 1000) % 60
+    if (this == 0L) return "0초"
+    val days = this / (1000 * 60 * 60 * 24)
+    val hours = (this % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    val minutes = (this % (1000 * 60 * 60)) / (1000 * 60)
+    val seconds = (this % (1000 * 60)) / 1000
     return buildString {
+        if (days != 0L) append("${days}일 ")
         if (hours != 0L) append("${hours}시간 ")
         if (minutes != 0L) append("${minutes}분 ")
         if (seconds != 0L) append(("${seconds}초"))
-    }
+    }.removeSuffix(" ")
 }
